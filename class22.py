1cba6e5422b5a45b675abca05ce9f0d31bea6c6c
varadhara <balajivaradarajan98@gmail.com>
2024-02-01 16:19:27 +0530

    newcode

---
+++ class22.py
@@ -0,0 +1,11 @@
+import math
+class AreaOfCircle:
+    def __init__(self,radius):
+        self.r=radius
+    def area(self):
+        return 2*self.r
+obj1=AreaOfCircle(5)
+obj1=AreaOfCircle(5)
+obj1.area()
+hello=obj1.area()
+print(hello)
