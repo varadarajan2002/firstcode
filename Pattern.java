5071f04a82e3d096826426afffa101d15585fe49
Varadarajan balaji <87330637+varadarajan2002@users.noreply.github.com>
2024-04-18 10:00:17 +0530

    Add files via upload
    pattern program using * in java

---
+++ Pattern.java
@@ -0,0 +1,12 @@
+public class Pattern{
+	public static void main(String args []){
+		int i,j;
+		int n=5;
+		for(i=0;i<=n;i++){
+			for(j=1;j<=i;j++){
+				System.out.print("*");
+				}
+			System.out.println();
+		    }
+}
+}
