b028cff71aced2f56a4032cb58b53caf13e320c4
Varadarajan balaji <87330637+varadarajan2002@users.noreply.github.com>
2024-06-24 21:46:53 +0530

    Add files via upload

---
+++ new11.py
@@ -0,0 +1,11 @@
+num=int(input("enter the time:"))
+
+if num==6:
+    print("good morning")
+elif num==12:
+    print("good after noon")
+elif num==16:
+    print("good evening")
+else:
+    print("good night")
+    
