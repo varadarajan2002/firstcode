60cca8a620a28ba7db6d9e0695f6f830337c57d9
Varadarajan balaji <87330637+varadarajan2002@users.noreply.github.com>
2024-06-24 21:43:45 +0530

    Add files via upload

---
+++ new7.py
@@ -0,0 +1,8 @@
+num=int(input("enter the number"))
+
+if num >0:
+    print("the number is a positive value")
+elif num==0:
+    print("the number is a zero value")
+else:
+    print("the number is a negative value") 
