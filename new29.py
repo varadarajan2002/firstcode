eef8706448cca1fd5ba8bf317d2212fc437535b1
Varadarajan balaji <87330637+varadarajan2002@users.noreply.github.com>
2024-06-26 20:58:24 +0530

    Add files via upload

---
+++ new29.py
@@ -0,0 +1,19 @@
+#get character of first position in a string
+
+n=input("enter the string")
+
+print(n[2:5])
+
+print(len(n))
+
+print(n[0])
+
+print(n.strip())
+
+print(n.lower())
+
+print(n.upper())
+
+print(n.replace("he","ha"))
+
+print(n.split())
