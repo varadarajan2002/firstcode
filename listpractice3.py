b4a05191dca4bada661c9f371e297f5b2ab38aad
Varadarajan balaji <87330637+varadarajan2002@users.noreply.github.com>
2024-07-01 21:18:14 +0530

    Add files via upload

---
+++ listpractice3.py
@@ -0,0 +1,13 @@
+#list comprehension even numbers
+
+a=[1,21,3,4,5,6,7,8,9,10]
+lst=[]
+
+for i in a:
+    if i%2==0:
+        lst.append(i)
+
+print(lst)
+a1=reversed(lst)
+for j in a1:
+    print(i)
