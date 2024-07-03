ccfcaf0e4ad83e304b5f18cd2ea8eb6d70f26bf7
Varadarajan balaji <87330637+varadarajan2002@users.noreply.github.com>
2024-07-03 19:40:34 +0530

    Add files via upload

---
+++ days.py
@@ -0,0 +1,15 @@
+date=eval(input())
+days=int(input())
+month=int(input())
+
+actualday=30
+actualmonth=12
+
+remaindate=actualday-days
+remainingmonth=actualmonth-month
+
+print("the number of days remaining is",remaindate,"the no of months is",remainingmonth)
+
+
+
+
